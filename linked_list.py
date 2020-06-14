4401cd9d2ccec0280ac5f490db21c791d6254b31
Erik Sultanaliev <ersul4ik@gmail.com>
2020-06-14 21:01:03 +0600

    Complete the logic for the delete_node method

--- linked_list.py
+++ linked_list.py
@@ -15,7 +15,9 @@ class ListNode:
             return None
         if node.next is None:
             raise ValueError('The given node will not be the tail')
-        
+        node.value = node.next.value
+        node.next = node.next.next
+
 
 if __name__ == '__main__':
     linked_list = ListNode(4)
@@ -27,3 +29,8 @@ if __name__ == '__main__':
     assert linked_list.next.value == 3
     assert linked_list.next.next.value == 2
     assert linked_list.next.next.next.value == 12
+
+    # Cover delete node method with assertions
+    ListNode.delete_node(linked_list.next)
+    assert linked_list.value == 4
+    assert linked_list.next.value == 2
