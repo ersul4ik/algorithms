8f29e9d0cd35a6db5e086e2a5650527923654e2e
Erik Sultanaliev <ersul4ik@gmail.com>
2020-06-14 21:57:50 +0600

    Modify reverse_string method by using recursion approach

--- reverse_string.py
+++ reverse_string.py
@@ -3,9 +3,11 @@ from typing import List
 
 class Solution:
     def reverse_string(self, s: List[str]) -> None:
-        for i in range(int(len(s)/2)):
-            s[i], s[-1 - i] = s[-1 - i], s[i]
-
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+        helper(0, len(s) - 1)
 
 
 if __name__ == '__main__':
